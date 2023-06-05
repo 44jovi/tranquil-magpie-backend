9d1434238be34177b6f2130bbfdbd6a9fca777a3
44jovi <kwong.jovi@gmail.com>
2023-06-05 09:42:21 +0100

    update(User): add Spring annotations/imports, add no-arg constructor. Change package

--- src/main/java/com/tranquilmagpie/springboot/rest/MainController.java
+++ src/main/java/com/tranquilmagpie/springboot/rest/MainController.java
@@ -1,6 +1,6 @@
 package com.tranquilmagpie.springboot.rest;
 
-import com.tranquilmagpie.domain.User;
+import com.tranquilmagpie.springboot.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 

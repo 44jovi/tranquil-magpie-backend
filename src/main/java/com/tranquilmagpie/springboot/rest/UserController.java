b438327f67203b50fc98076f31e55d48b1340d08
44jovi <kwong.jovi@gmail.com>
2023-06-05 14:06:27 +0100

    update(UserController): add @PostMapping for 'users/create'

--- src/main/java/com/tranquilmagpie/springboot/rest/UserController.java
+++ src/main/java/com/tranquilmagpie/springboot/rest/UserController.java
@@ -2,7 +2,11 @@ package com.tranquilmagpie.springboot.rest;
 
 import com.tranquilmagpie.springboot.domain.User;
 import com.tranquilmagpie.springboot.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +24,12 @@ public class UserController {
     @GetMapping("/users/getAll")
     public List<User> getAll() {
         return this.service.getAll();
+    }
+
+
+    @PostMapping("/users/create")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User createdUser = this.service.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 }

6e5e78eb29cf5907765feafa517f44676e8e8913
44jovi <kwong.jovi@gmail.com>
2023-06-05 09:49:15 +0100

    update(UserController, application.properties): add UserService to allow getAll(). Set default schema in Spring config

--- src/main/java/com/tranquilmagpie/springboot/rest/UserController.java
+++ src/main/java/com/tranquilmagpie/springboot/rest/UserController.java
@@ -1,15 +1,24 @@
 package com.tranquilmagpie.springboot.rest;
 
+import com.tranquilmagpie.springboot.domain.User;
+import com.tranquilmagpie.springboot.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// TODO: constructor with UserService parameter
+import java.util.List;
 
 @RestController
 public class UserController {
 
+    private UserService service;
+
+    public UserController(UserService service) {
+        super();
+        this.service = service;
+    }
+
     @GetMapping("/users/getAll")
-    public String tempMsg() {
-        return "return all users - WIP";
+    public List<User> getAll() {
+        return this.service.getAll();
     }
 }

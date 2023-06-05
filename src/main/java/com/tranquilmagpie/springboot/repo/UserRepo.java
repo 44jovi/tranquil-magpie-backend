a6cc344868823f7ab3bd46e82a593ee1c87b46e6
44jovi <kwong.jovi@gmail.com>
2023-06-05 09:44:22 +0100

    add(UserRepo): default extension of JpaRepository

---
+++ src/main/java/com/tranquilmagpie/springboot/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.tranquilmagpie.springboot.repo;
+
+import com.tranquilmagpie.springboot.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long>{
+    // JpaRepository provides standard CRUD functions
+    // Insert custom methods here
+}

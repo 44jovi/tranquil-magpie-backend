99f30c99cc93880fa205148ccf63b3bf66e21a9d
44jovi <kwong.jovi@gmail.com>
2023-06-05 14:02:49 +0100

    update(User, MainController): change 'dob' data type to Date. Add strategy to @GeneratedValue. Remove example user from MainController

--- src/main/java/com/tranquilmagpie/springboot/rest/MainController.java
+++ src/main/java/com/tranquilmagpie/springboot/rest/MainController.java
@@ -1,15 +1,13 @@
 package com.tranquilmagpie.springboot.rest;
 
-import com.tranquilmagpie.springboot.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class MainController {
-    User userExample1 = new User(44, "test@test.com", "username123", "Wendy", "Kit", "1904-03-02");
-
     @GetMapping("/")
     public String welcome() {
-        return "welcome | example user: " + userExample1;
+        return "welcome";
     }
 }

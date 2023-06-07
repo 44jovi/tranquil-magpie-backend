bda69ffe00a616ff26403d072fd2d6f487ea9263
44jovi <kwong.jovi@gmail.com>
2023-06-07 17:27:08 +0100

    generate files and code via openapi-generator

---
+++ src/main/gen/com/tranquilmagpie/user/OpenApiGeneratorApplication.java
@@ -0,0 +1,30 @@
+package com.tranquilmagpie.user;
+
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
+
+@SpringBootApplication(
+    nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+)
+@ComponentScan(
+    basePackages = {"com.tranquilmagpie.user", "com.tranquilmagpie.user.api" , "com.tranquilmagpie.user.config"},
+    nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+)
+public class OpenApiGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiGeneratorApplication.class, args);
+    }
+
+    @Bean(name = "com.tranquilmagpie.user.OpenApiGeneratorApplication.jsonNullableModule")
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
+    }
+
+}

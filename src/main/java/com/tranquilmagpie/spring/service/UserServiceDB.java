64d6659bcb2120ee24e79c5465e68ee428d37e38
44jovi <kwong.jovi@gmail.com>
2023-06-19 13:38:43 +0100

    update(UserServiceDB): update getOneById to work with repo.findByUuid

--- src/main/java/com/tranquilmagpie/spring/service/UserServiceDB.java
+++ src/main/java/com/tranquilmagpie/spring/service/UserServiceDB.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Primary
 @Service
@@ -24,12 +26,11 @@ public class UserServiceDB implements UserService {
     public List<User> getAll() {
         return this.repo.findAll();
     }
-
+    // TODO: check use of Optional - consider Optional<User>
     @Override
-    public User getOneById(Long id) {
+    public Optional getOneById(UUID id) {
         // TODO: review how to handle Optional
-        // WIP - return null if entity not found
-        return this.repo.findById(id).orElse(null);
+        return this.repo.findByUuid(id);
     }
 
     @Override
@@ -37,38 +38,39 @@ public class UserServiceDB implements UserService {
         return this.repo.save(user);
     }
 
-    @Override
-    public User deleteOneById(Long id) {
-        User selectedUser = this.getOneById(id);
-        this.repo.deleteById(id);
-        return selectedUser;
-    }
+    // TODO: fix this method
+//    @Override
+//    public Optional<User> deleteOneById(UUID id) {
+////        Optional<User> selectedUser = this.getOneById(id);
+//        return this.repo.deleteByUuid(id);
+////        return selectedUser;
+//    }
 
-    @Override
-    public User patchOneById(Long id, User user) {
-
-        String email = user.getEmail();
-        String username = user.getUsername();
-        String firstName = user.getFirstName();
-        String lastName = user.getLastName();
-        LocalDate dob = user.getDob();
-
-        User selectedUser = this.getOneById(id);
-
-//        TODO: use ternary statements?
-        if (email != null)
-            selectedUser.setEmail(email);
-        if (username != null)
-            selectedUser.setUsername(username);
-        if (firstName != null)
-            selectedUser.setFirstName(firstName);
-        if (lastName != null)
-            selectedUser.setLastName(lastName);
-        if (dob != null)
-            selectedUser.setDob(dob);
-
-        //  .save - JPA calls entityManager.merge if entity already exists (otherwise entityManager.persist)
-        return this.repo.save(selectedUser);
-    }
+//    @Override
+//    public User patchOneById(UUID id, User user) {
+//
+//        String email = user.getEmail();
+//        String username = user.getUsername();
+//        String firstName = user.getFirstName();
+//        String lastName = user.getLastName();
+//        LocalDate dob = user.getDob();
+//
+//        User selectedUser = this.getOneById(id);
+//
+////        TODO: use ternary statements?
+//        if (email != null)
+//            selectedUser.setEmail(email);
+//        if (username != null)
+//            selectedUser.setUsername(username);
+//        if (firstName != null)
+//            selectedUser.setFirstName(firstName);
+//        if (lastName != null)
+//            selectedUser.setLastName(lastName);
+//        if (dob != null)
+//            selectedUser.setDob(dob);
+//
+//        //  .save - JPA calls entityManager.merge if entity already exists (otherwise entityManager.persist)
+//        return this.repo.save(selectedUser);
+//    }
 
 }

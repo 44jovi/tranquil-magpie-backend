69ecd3140173be931d68dc9881162961f4c3c5cd
44jovi <kwong.jovi@gmail.com>
2023-06-21 14:14:39 +0100

    update(UserRepo, UserRepoTest): add @Transactional to interface. Add testDeleteByUuid

--- src/main/java/com/tranquilmagpie/spring/repo/UserRepo.java
+++ src/main/java/com/tranquilmagpie/spring/repo/UserRepo.java
@@ -3,6 +3,7 @@ package com.tranquilmagpie.spring.repo;
 import com.tranquilmagpie.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -15,6 +16,8 @@ public interface UserRepo extends JpaRepository<User, UUID> {
     // TODO: review method name
     Optional<User> findByUuid(UUID id);
 
-    // TODO: remove from Swagger UI render
+    // TODO: remove from Swagger UI render?
+    // Current purpose of @Transactional in this interface is for tests only
+    @Transactional
     Long deleteByUuid(UUID id);
 }
--- src/test/java/com/tranquilmagpie/spring/repo/UserRepoTest.java
+++ src/test/java/com/tranquilmagpie/spring/repo/UserRepoTest.java
@@ -21,18 +21,34 @@ public class UserRepoTest {
     @Test
     public void testFindByUuid() {
 
-    // TODO: review alternative as this is dependent on an entity with this UUID in the database
-    UUID uuid = UUID.fromString("dfb68f04-b0ac-42a9-b9a3-97f843791f4c");
+        // TODO: review alternative as this is dependent on an entity with this UUID in the database
+        UUID uuid = UUID.fromString("dfb68f04-b0ac-42a9-b9a3-97f843791f4c");
 
-    // TODO: review usage of .isPresent()
-    User user = userRepo.findByUuid(uuid).get();
+        // TODO: review usage of .isPresent()
+        User user = userRepo.findByUuid(uuid).get();
 
-    assertEquals(user.getId(), uuid);
-    assertEquals("joe1@test.com", user.getEmail());
-    assertEquals("joe1", user.getUsername());
-    assertEquals("joe", user.getFirstName());
-    assertEquals("joeman", user.getLastName());
-    assertEquals(LocalDate.parse("1900-12-31"), user.getDob());
+        assertEquals(user.getId(), uuid);
+        assertEquals("joe1@test.com", user.getEmail());
+        assertEquals("joe1", user.getUsername());
+        assertEquals("joe", user.getFirstName());
+        assertEquals("joeman", user.getLastName());
+        assertEquals(LocalDate.parse("1900-12-31"), user.getDob());
+    }
+
+    @Test
+    public void testDeleteByUuid() {
+        User user = new User();
+        user.setEmail("ross1@test.com");
+        user.setUsername("ross1");
+        user.setFirstName("ross");
+        user.setLastName("geller");
+        user.setDob(LocalDate.parse("1967-10-18"));
+
+        User savedUser = userRepo.save(user);
+
+        Long dbRowsDeleted = userRepo.deleteByUuid(savedUser.getId());
+
+        assertEquals(1, dbRowsDeleted);
     }
 
 }

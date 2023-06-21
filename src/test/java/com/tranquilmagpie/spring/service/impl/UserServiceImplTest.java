434873e60df456b2c6315316c912057ef70df113
44jovi <kwong.jovi@gmail.com>
2023-06-21 17:02:49 +0100

    update(UserServiceImplTest): add @BeforeEach and testSetup. Update testGetAll

--- src/test/java/com/tranquilmagpie/spring/service/impl/UserServiceImplTest.java
+++ src/test/java/com/tranquilmagpie/spring/service/impl/UserServiceImplTest.java
@@ -5,19 +5,29 @@ import static org.mockito.Mockito.*;
 
 import com.tranquilmagpie.spring.model.User;
 import com.tranquilmagpie.spring.repo.UserRepo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 class UserServiceImplTest {
+    List<User> usersList;
+    User user1;
+    User user2;
+
+    @BeforeEach
+    public void testSetup() {
+        usersList = new ArrayList<>();
+        user1 = new User("phoebe1@test.com", "phoebe1", "phoebe", "buffay", LocalDate.parse("1966-02-16"));
+        user2 = new User("monica1@test.com", "monica1", "monica", "geller", LocalDate.parse("1969-01-01"));
+        usersList.add(user1);
+        usersList.add(user2);
+    }
 
     @Test
     public void testGetAll() {
-        List<User> usersList = new ArrayList<>();
-        usersList.add(new User());
-        usersList.add(new User());
-
         UserRepo UserRepoMock = mock(UserRepo.class);
         when(UserRepoMock.findAll()).thenReturn(usersList);
 
@@ -26,7 +36,9 @@ class UserServiceImplTest {
         List<User> usersFound = impl.getAll();
 
         assertEquals(User.class, usersFound.get(0).getClass());
+        assertEquals("phoebe1", usersFound.get(0).getUsername());
         assertEquals(User.class, usersFound.get(1).getClass());
+        assertEquals("monica1", usersFound.get(1).getUsername());
     }
 
 }

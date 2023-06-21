ec8333de57fa4fb50d8b1fc8cefa8f40f050aa71
44jovi <kwong.jovi@gmail.com>
2023-06-21 10:10:58 +0100

    update(UserTest): set @BeforeEach. Separate current tests

--- src/test/java/com/tranquilmagpie/spring/UserTest.java
+++ src/test/java/com/tranquilmagpie/spring/UserTest.java
@@ -1,31 +1,48 @@
 package com.tranquilmagpie.spring;
 
 import com.tranquilmagpie.spring.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public class UserTest {
-    @Test
-    public void testUser() {
 
-        User user = new User();
+    User user1 = new User();
+
+    @BeforeEach
+    void setup() {
         // Alternative method: UUID.randomUUID()
-        user.setUuid(UUID.fromString("446d5db4-70dc-433e-a4f8-c2bbec0d25ab"));
-        user.setEmail("joey@test.com");
-        user.setFirstName("joey");
-        user.setLastName("tribbiani");
-        user.setDob(LocalDate.parse("1968-01-01"));
+        user1.setUuid(UUID.fromString("446d5db4-70dc-433e-a4f8-c2bbec0d25ab"));
+        user1.setEmail("joey@test.com");
+        user1.setFirstName("joey");
+        user1.setLastName("tribbiani");
+        user1.setDob(LocalDate.parse("1968-01-01"));
+    }
 
-        assertEquals(UUID.fromString("446d5db4-70dc-433e-a4f8-c2bbec0d25ab"), user.getId());
-        assertEquals("joey@test.com", user.getEmail());
-        assertEquals("joey", user.getFirstName());
-        assertEquals("tribbiani", user.getLastName());
-        assertEquals(LocalDate.parse("1968-01-01"), user.getDob());
+    @Test
+    public void testGetters() {
+        assertEquals(UUID.fromString("446d5db4-70dc-433e-a4f8-c2bbec0d25ab"), user1.getId());
+        assertEquals("joey@test.com", user1.getEmail());
+        assertEquals("joey", user1.getFirstName());
+        assertEquals("tribbiani", user1.getLastName());
+        assertEquals(LocalDate.parse("1968-01-01"), user1.getDob());
+    }
 
+    @Test
+    public void testToString() {
+        assertEquals(
+                "class User {\n" +
+                        "    id: 446d5db4-70dc-433e-a4f8-c2bbec0d25ab\n" +
+                        "    email: joey@test.com\n" +
+                        "    username: null\n" +
+                        "    firstName: joey\n" +
+                        "    lastName: tribbiani\n" +
+                        "    dob: 1968-01-01\n" +
+                        "}"
+                , user1.toString());
     }
 }

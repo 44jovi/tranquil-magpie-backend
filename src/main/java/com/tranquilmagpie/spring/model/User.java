91cc0d41d49465ac3cb1426ae286374337cd435f
44jovi <kwong.jovi@gmail.com>
2023-06-29 10:52:19 +0100

    update(User, UserTest): remove redundant methods generated by Lombok annotations

--- src/main/java/com/tranquilmagpie/spring/model/User.java
+++ src/main/java/com/tranquilmagpie/spring/model/User.java
@@ -1,8 +1,5 @@
 package com.tranquilmagpie.spring.model;
 
-import java.util.Objects;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import java.time.LocalDate;
@@ -14,8 +11,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-
-import jakarta.validation.Valid;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -53,7 +48,7 @@ public class User {
     // TODO: review field name
     private String passwordHash;
 
-    // Current purpose of this constructor is for tests
+    // TODO: Update tests to use @Builder. Current purpose of this constructor is for tests
     public User(String email, String username, String firstName, String lastName, LocalDate dob, String passwordHash) {
         this.email = email;
         this.username = username;
@@ -63,157 +58,4 @@ public class User {
         this.passwordHash = passwordHash;
     }
 
-    /**
-     * Get uuid
-     *
-     * @return uuid
-     */
-
-    @Schema(name = "uuid", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    @JsonProperty("uuid")
-    public UUID getUuid() {
-        return this.uuid;
-    }
-
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    /**
-     * Get email
-     *
-     * @return email
-     */
-
-    @Schema(name = "email", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    @JsonProperty("email")
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * Get username
-     *
-     * @return username
-     */
-
-    @Schema(name = "username", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    @JsonProperty("username")
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * Get firstName
-     *
-     * @return firstName
-     */
-
-    @Schema(name = "firstName", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    @JsonProperty("firstName")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Get lastName
-     *
-     * @return lastName
-     */
-
-    @Schema(name = "lastName", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    @JsonProperty("lastName")
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Get dob
-     *
-     * @return dob
-     */
-    @Valid
-    @Schema(name = "dob", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    @JsonProperty("dob")
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    @Valid
-    @Schema(name = "passwordHash", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    @JsonProperty("passwordHash")
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return Objects.equals(this.uuid, user.uuid) &&
-                Objects.equals(this.email, user.email) &&
-                Objects.equals(this.username, user.username) &&
-                Objects.equals(this.firstName, user.firstName) &&
-                Objects.equals(this.lastName, user.lastName) &&
-                Objects.equals(this.dob, user.dob);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(uuid, email, username, firstName, lastName, dob);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class User {\n");
-        sb.append("    uuid: ").append(toIndentedString(uuid)).append("\n");
-        sb.append("    email: ").append(toIndentedString(email)).append("\n");
-        sb.append("    username: ").append(toIndentedString(username)).append("\n");
-        sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
-        sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
-        sb.append("    dob: ").append(toIndentedString(dob)).append("\n");
-        sb.append("}");
-        return sb.toString();
-    }
-
-    /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
-     */
-    private String toIndentedString(Object o) {
-        if (o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
-    }
 }
--- src/test/java/com/tranquilmagpie/spring/model/UserTest.java
+++ src/test/java/com/tranquilmagpie/spring/model/UserTest.java
@@ -44,14 +44,7 @@ class UserTest {
     @Test
     void testToString() {
         assertEquals(
-                "class User {\n" +
-                        "    uuid: 446d5db4-70dc-433e-a4f8-c2bbec0d25ab\n" +
-                        "    email: joey1@test.com\n" +
-                        "    username: joey1\n" +
-                        "    firstName: joey\n" +
-                        "    lastName: tribbiani\n" +
-                        "    dob: 1968-01-09\n" +
-                        "}"
+                "User(uuid=446d5db4-70dc-433e-a4f8-c2bbec0d25ab, email=joey1@test.com, username=joey1, firstName=joey, lastName=tribbiani, dob=1968-01-09, passwordHash=null)"
                 , user1.toString());
     }
 
@@ -64,8 +57,8 @@ class UserTest {
 
     @Test
     void testHashCode() {
-        assertEquals(231396512, user1.hashCode());
-        assertEquals(-2031932401, user2.hashCode());
+        assertEquals(-1253687397, user1.hashCode());
+        assertEquals(763840416, user2.hashCode());
     }
 
 }

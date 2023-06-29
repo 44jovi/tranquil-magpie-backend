70ea2023831182ba2766b416f202fedf4641d61f
44jovi <kwong.jovi@gmail.com>
2023-06-29 14:03:16 +0100

    update(pom, JwtService): add JWT dependencies. getSigningKey, extractAllClaims

--- src/main/java/com/tranquilmagpie/spring/config/JwtService.java
+++ src/main/java/com/tranquilmagpie/spring/config/JwtService.java
@@ -1,10 +1,29 @@
 package com.tranquilmagpie.spring.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SIGNING_KEY = System.getenv("TRANQUIL_MAGPIE_SK");
+
     public String extractUsername(String jwt) {
         return "WIP - TBC";
+    }
+
+    private Claims extractAllClaims(String jwt) {
+        // Signing key (secret) signs the JWT and used to verify the signature
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(jwt).getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyAsBytes = Decoders.BASE64.decode(SIGNING_KEY);
+        return Keys.hmacShaKeyFor(keyAsBytes);
     }
 }

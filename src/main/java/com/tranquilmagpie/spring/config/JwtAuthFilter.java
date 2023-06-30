f5eab9d12a1201ea1a7037fc7a46d6f3609ba021
44jovi <kwong.jovi@gmail.com>
2023-06-30 12:01:46 +0100

    update(JwtAuthFilter): complete doFilterInternal

--- src/main/java/com/tranquilmagpie/spring/config/JwtAuthFilter.java
+++ src/main/java/com/tranquilmagpie/spring/config/JwtAuthFilter.java
@@ -6,9 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,6 +22,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(
             // Intercept each response/request to allow extraction/manipulation of the data
@@ -43,7 +46,21 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         // Check user exists in token and not already authenticated
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if (jwtService.isJwtValid(jwt, userDetails)) {
+                // Token required by SecurityContextHolder to update the security context
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        // TODO: review / update
+                        null,
+                        userDetails.getAuthorities()
+                );
+                // Add further information to the token from client request
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // Mark user as authenticated in the security context
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
-
+        // Pass request/response to the next filter in chain
+        filterChain.doFilter(request, response);
     }
 }

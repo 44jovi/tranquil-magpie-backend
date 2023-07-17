905254bf53701a02b2538204ffb65f396f7af36f
44jovi <kwong.jovi@gmail.com>
2023-07-17 13:06:01 +0100

    update(UserRepo): remove deleteByUuid()

--- src/main/java/com/tranquilmagpie/spring/repo/UserRepo.java
+++ src/main/java/com/tranquilmagpie/spring/repo/UserRepo.java
@@ -3,7 +3,6 @@ package com.tranquilmagpie.spring.repo;
 import com.tranquilmagpie.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -18,9 +17,4 @@ public interface UserRepo extends JpaRepository<User, UUID> {
     Optional<User> findById(UUID id);
 
     Optional<User> findByUsername(String username);
-
-    // Current purpose of @Transactional in this interface is for tests only
-    // TODO: review how to test without using @Transactional
-    @Transactional
-    Long deleteByUuid(UUID id);
 }

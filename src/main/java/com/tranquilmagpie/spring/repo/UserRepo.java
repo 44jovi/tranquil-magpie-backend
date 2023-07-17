0b05df82643fd3377bec73861e687205bd24d140
44jovi <kwong.jovi@gmail.com>
2023-07-17 13:40:02 +0100

    remove comment(UserRepo)

--- src/main/java/com/tranquilmagpie/spring/repo/UserRepo.java
+++ src/main/java/com/tranquilmagpie/spring/repo/UserRepo.java
@@ -12,7 +12,6 @@ public interface UserRepo extends JpaRepository<User, UUID> {
     // JpaRepository provides standard CRUD functions
     // Custom methods:
 
-    // TODO: review method name
     @Override
     Optional<User> findById(UUID id);
 

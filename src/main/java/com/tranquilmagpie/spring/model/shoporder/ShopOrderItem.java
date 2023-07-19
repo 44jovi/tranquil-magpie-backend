7f91d717ebe44d982656877b8c061afe8f843ca2
44jovi <kwong.jovi@gmail.com>
2023-07-19 16:49:47 +0100

    create(ShopOrderItem, ShopOrderItemKey): link both classes via @Embeddable / @EmbeddedId

---
+++ src/main/java/com/tranquilmagpie/spring/model/shoporder/ShopOrderItem.java
@@ -0,0 +1,25 @@
+package com.tranquilmagpie.spring.model.shoporder;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "shop_order_item")
+@Schema(name = "shop_order_item", description = "item (product) in user's shop order")
+public class ShopOrderItem {
+
+    @EmbeddedId
+    private ShopOrderItemKey id;
+    private Integer qty;
+
+}

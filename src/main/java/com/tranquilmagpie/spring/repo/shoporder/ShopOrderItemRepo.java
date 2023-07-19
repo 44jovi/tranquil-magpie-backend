d22b668eabab0636c5430c6d600d174da0b7e7ff
44jovi <kwong.jovi@gmail.com>
2023-07-19 21:42:10 +0100

    amend(ShopOrderItemRepo): 'findAllByShopOrderId' to 'findByIdShopOrderId'

--- src/main/java/com/tranquilmagpie/spring/repo/shoporder/ShopOrderItemRepo.java
+++ src/main/java/com/tranquilmagpie/spring/repo/shoporder/ShopOrderItemRepo.java
@@ -1,17 +1,17 @@
 package com.tranquilmagpie.spring.repo.shoporder;
 
 import com.tranquilmagpie.spring.model.shoporder.ShopOrderItem;
-import com.tranquilmagpie.spring.model.shoporder.ShopOrderItemKey;
+import com.tranquilmagpie.spring.model.shoporder.ShopOrderItemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ShopOrderItemRepo extends JpaRepository<ShopOrderItem, ShopOrderItemKey> {
+public interface ShopOrderItemRepo extends JpaRepository<ShopOrderItem, ShopOrderItemId> {
 
-    Optional<List<ShopOrderItem>> findAllByShopOrderId(UUID id);
+    // TODO: return optional
+    List<ShopOrderItem> findByIdShopOrderId(UUID id);
 
 }

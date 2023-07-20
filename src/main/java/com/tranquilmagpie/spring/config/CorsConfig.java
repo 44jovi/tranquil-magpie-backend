23e301cbd2e61eae88e40564bf880c8d8df26521
44jovi <kwong.jovi@gmail.com>
2023-07-20 16:39:27 +0100

    create(CorsConfig)

---
+++ src/main/java/com/tranquilmagpie/spring/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.tranquilmagpie.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+//    TODO: review how to configure this properly
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}

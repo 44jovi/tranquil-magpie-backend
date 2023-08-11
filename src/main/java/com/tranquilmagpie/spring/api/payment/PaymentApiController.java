afd8b91b15d34a338963a43dc28e67816fa51b33
44jovi <kwong.jovi@gmail.com>
2023-08-11 14:31:34 +0100

    update(PaymentApiController): delete createPaymentIntent. add createCheckoutSession.

--- src/main/java/com/tranquilmagpie/spring/api/payment/PaymentApiController.java
+++ src/main/java/com/tranquilmagpie/spring/api/payment/PaymentApiController.java
@@ -2,15 +2,19 @@ package com.tranquilmagpie.spring.api.payment;
 
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
-import com.stripe.model.PaymentIntent;
-import com.stripe.param.PaymentIntentCreateParams;
+import com.stripe.model.checkout.Session;
+
+import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/payment")
@@ -19,24 +23,54 @@ public class PaymentApiController {
     @Value("${stripe.api.key}")
     private String apiKey;
 
-    @PostMapping("/payment-intent")
-    public ResponseEntity<CreatePaymentIntentResponse> createPaymentIntent(@RequestBody CreatePaymentIntentRequest req) throws StripeException {
+    @PostMapping("/create-checkout-session")
+    public ResponseEntity<String> createCheckoutSession() throws StripeException {
         Stripe.apiKey = apiKey;
 
-        PaymentIntentCreateParams params = new PaymentIntentCreateParams.Builder()
-                .setAmount(req.getAmount())
-                .setCurrency(req.getCurrency())
-                .setDescription(req.getDescription())
-                .build();
+        List<SessionCreateParams.LineItem> lineItems = new ArrayList<>();
 
-        PaymentIntent paymentIntent = PaymentIntent.create(params);
-
-        CreatePaymentIntentResponse response = new CreatePaymentIntentResponse(
-                paymentIntent.getId(),
-                paymentIntent.getClientSecret()
+        // TODO: loop through list of s-o-i and add them to lineItems
+        lineItems.add(
+                SessionCreateParams.LineItem.builder()
+                        .setPriceData(
+                                SessionCreateParams.LineItem.PriceData.builder()
+                                        .setCurrency("gbp")
+                                        .setProductData(
+                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                        // TODO: insert product name from s-o-i
+                                                        .setName("blah")
+                                                        .build()
+                                        )
+                                        // TODO: insert price from s-o-i
+                                        .setUnitAmount(2000L)
+                                        // TODO: review .setTaxBehavior
+                                        .build()
+                        )
+                        // For reference only. Do not allow customer to amend shop order items at checkout page
+                        // .setAdjustableQuantity(
+                        //     SessionCreateParams.LineItem.AdjustableQuantity.builder()
+                        //     .setEnabled(true)
+                        //     .setMinimum(1L)
+                        //     .setMaximum(10L)
+                        // .build()
+                        //                      )
+                        // TODO: insert qty from s-o-i
+                        .setQuantity(3L)
+                        .build()
         );
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        SessionCreateParams params = SessionCreateParams.builder()
+                // previously .setLineItems()
+                .addAllLineItem(lineItems)
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("https://example.com/success")
+                .setCancelUrl("https://example.com/cancel")
+                .build();
+
+        Session session = Session.create(params);
+        System.out.println(session.getUrl());
+        String checkoutUrl = session.getUrl();
+        return new ResponseEntity<>(checkoutUrl, HttpStatus.OK);
     }
 
 }

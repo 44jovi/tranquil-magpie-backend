f9313c8a3412c79ab9dbd5b60ac680477e20cf40
44jovi <kwong.jovi@gmail.com>
2023-08-20 07:48:02 +0100

    update(ProductTest): setUp - refactor

--- src/test/java/com/tranquilmagpie/spring/model/ProductTest.java
+++ src/test/java/com/tranquilmagpie/spring/model/ProductTest.java
@@ -16,18 +16,18 @@ class ProductTest {
     @BeforeEach
     void setUp() {
         product1.setId(UUID.randomUUID());
-        product1.setName("product 1");
-        product1.setDescription("product 1 description");
+        product1.setName("test product 1");
+        product1.setDescription("test product 1 description");
         product1.setPrice(new BigDecimal("12.34"));
         product1.setStockQty(1);
-        product1.setImageFilename("product-1-filename");
+        product1.setImageFilename("test-product-1-filename");
 
         product2.setId(UUID.randomUUID());
-        product2.setName("product 2");
-        product2.setDescription("product 1 description");
+        product2.setName("test product 2");
+        product2.setDescription("test product 2 description");
         product2.setPrice(new BigDecimal("56.78"));
         product2.setStockQty(2);
-        product2.setImageFilename("product-2-filename");
+        product2.setImageFilename("test-product-2-filename");
     }
 
     @Test

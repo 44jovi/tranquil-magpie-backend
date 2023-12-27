830ff58cc7a5bf1c7aeaf37cfc6b11b9188ca391
44jovi <kwong.jovi@gmail.com>
2023-12-27 21:43:04 +0000

    update(db-data-insert.sql): user_address

--- src/main/resources/db/db-data-insert.sql
+++ src/main/resources/db/db-data-insert.sql
@@ -1,6 +1,7 @@
 -- IMPORTANT: Set 'search_path' to the relevant schema
 SET search_path TO backend;
 
+-- USER
 -- TODO: refactor 'users' to 'user'
 insert into users (
     id,	email, username, given_name, family_name, dob, password, role
@@ -15,3 +16,18 @@ insert into users (
 	'not-yet-hashed-password',
 	'USER'
 	);
+
+-- USER_ADDRESS
+insert into user_address (
+    "id",
+    city,
+    line_1,
+    postcode,
+    user_id)
+values (
+    '28836c68-d464-4c11-ae35-32da5127ccff',
+    'Tranquilton',
+    '1 Barry Street',
+    'ABC1D23',
+    '2e53a105-09a4-4e9a-8862-f16558547cc7')
+;
